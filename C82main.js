4767b95a863d1a44f218b2a7334bcb3127c8d877
Kshitij Lahoti <78255433+Kshtij-7@users.noreply.github.com>
2021-05-12 16:00:43 +0530

    Update C82main.js

--- C82main.js
+++ C82main.js
@@ -4,17 +4,17 @@ var lastY = 0
 var mouseEvent = "empty"
 
 var ref = canvas.getContext("2d")
-color = "black"
-width = 10
-radius = 5
+colorone = "black"
+widthone = 10
+radiusone = 5
 
 canvas.addEventListener("mousedown", my_mousedown)
 
 function my_mousedown(e) {
-    color = document.getElementById("color").value
-    width = document.getElementById("width").value
-    radius = document.getElementById("radius").value
-    console.log(`Color is ${color}, Width is ${width}`)
+    colorone = document.getElementById("color").value
+    widthone = document.getElementById("width").value
+    radiusone = document.getElementById("radius").value
+    console.log(`Color is ${colorone}, Width is ${widthone}`)
     mouseEvent = "mouseDown";
 }
 
@@ -26,9 +26,9 @@ function my_mousemove(e) {
     
     if(mouseEvent == "mouseDown") {
         ref.beginPath();
-        ref.strokeStyle = color;
-        ref.lineWidth = width;
-        ref.arc(currentX, currentY, radius, 0, 2*Math.PI)
+        ref.strokeStyle = colorone;
+        ref.lineWidth = widthone;
+        ref.arc(currentX, currentY, radiusone, 0, 2*Math.PI)
         ref.stroke();
         
     }
